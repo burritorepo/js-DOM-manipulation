62b186f76b9ed937366bf7b0574aa9aec2e9406e
hacodeml <hacodeml@gmail.com>
2019-04-09 17:01:13 -0500

    modify js and add a new html

--- main.js
+++ main.js
@@ -2,12 +2,26 @@ const list = document.querySelectorAll("input[type=text]");
 const chose = document.querySelectorAll("select");
 const button = document.querySelector(".btn");
 
-button.addEventListener("click", function(e) {
+button.addEventListener("click", function (e) {
   e.preventDefault();
-  chose.forEach(function(option) {
-    console.log(option.value);
+  chose.forEach(function (option) {
+    let newelement = '';
+    console.log(option.id);
+    if (option.id === "element") {
+      if (option.value === "p") {
+        newelement = document.createElement(option.value);
+        document.body.appendChild(newelement);
+      }
+    }
+
+    console.log('newElement', newElement)
+    newelement.textContent = document.querySelector('.addtext').value;
+    newelement.style.backgroundColor = document.querySelector('.addcolor').value;
+    newelement.classList.add(document.querySelector('.addclass').value);
   });
-  list.forEach(function(item) {
-    console.log(item.value);
-  });
-});
+})
+
+
+
+
+
