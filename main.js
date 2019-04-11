7a651419b7cfb0b904c50079a2961c74774f76f9
Harold Mantilla <hacodeml@gmail.com>
2019-04-11 06:55:00 -0500

    add element with style and create new select option in 'inside this element...' input field

--- main.js
+++ main.js
@@ -1,35 +1,43 @@
 /* Función para crear objeto "element" que contiene valores del formulario */
 function getValues() {
   let element = {
-    create: document.querySelector('.js-addelement').value,
-    text: document.querySelector('.js-addtext').value,
-    color: document.querySelector('.js-addcolor').value,
-    class: document.querySelector('.js-addclass').value,
-    fontsize: document.querySelector('.js-font').value,
-    container: document.querySelector(`${document.querySelector('.js-element').value}`)
+    create: document.querySelector(".js-addelement").value,
+    text: document.querySelector(".js-addtext").value,
+    css: document.querySelector(".js-addcolor").value,
+    class: document.querySelector(".js-addclass").value,
+    fontsize: document.querySelector(".js-font").value,
+    container: document.querySelector(`${document.querySelector(".js-element").value}`)
   };
   return element;
 }
 
-function createElement(x) {
-  let newElement = document.createElement(x.create);
-  console.log('nuevo', newElement);
+function createElement(formvalues) {
+  let newElement = document.createElement(formvalues.create);
   return newElement;
 }
 
-function insert(elementValues, newElement) {
-  let container = elementValues.container;
+function insert(formvalues, newElement) {
+  let container = formvalues.container;
+  let text = document.createTextNode(`${formvalues.text}`);
   container.appendChild(newElement);
-  console.log('container', container);
+  newElement.appendChild(text);
+  newElement.style.cssText = `${formvalues.css}`;
+}
+
+function addElemenTypetoList(formvalues) {
+  let newoption = document.createElement("option");
+  let optiontext = document.createTextNode(`${formvalues.create}`)
+  newoption.appendChild(optiontext);
+  document.querySelector('#inside').appendChild(newoption);
 }
 
 
-let button = document.querySelector('button');
+let button = document.querySelector(".js-btn");
 
-button.addEventListener('click', function (e) {
-  console.log('click');
+button.addEventListener('click', (e) => {
   e.preventDefault();
   let formvalues = getValues();
   let newelement = createElement(formvalues);
   insert(formvalues, newelement);
-})
+  addElemenTypetoList(formvalues);
+});   
